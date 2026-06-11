d821a40183b8d2a3081cef8df6b0908d447c5e53
Kevin Tang <ctan1345@gmail.com>
2026-06-11 19:56:47 +1000

    Updated test runner

--- packages/sdk/src/testing/definitions.ts
+++ packages/sdk/src/testing/definitions.ts
@@ -1,4 +1,4 @@
-export type TestRunner = "reducer" | "embedded" | "browser";
+export type TestRunner = "reducer" | "remote" | "browser";
 
 export type InteractionDescriptorLike = {
   interactionId?: string;
